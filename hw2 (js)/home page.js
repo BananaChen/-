26c12b1ca057870b38ddf9f6572b260e5d20c42b
BananaChen <chenssuchi09@gmail.com>
2017-10-18 03:07:04 +0800

    Add files via upload

---
+++ hw2 (js)/home page.js
@@ -0,0 +1,50 @@
+var loaded = 0;
+$(document).ready(function(){
+  $('td').animate({'background-position': '0px'}, 1400);
+  $('td').animate({'background-position': '370px'}, 1400);
+  setTimeout(function(){
+    $('p').animate({'opacity': '1'}, 'slow');
+  }, 2100);
+  setTimeout(function(){
+    $("td").hover(
+      function(){
+          $(this).find('img').animate({'opacity': '1'}, 400)//ask why find img
+      },
+      function() {
+          $(this).find('img').animate({'opacity': '0'}, 400)
+      }
+    )
+    //$('td').css('background-position', '-370px');
+    loaded = 1;
+  }, 2500);
+})
+
+function nextPage(website){
+  if(loaded == 1){
+    $('td').css('background-position', '-370px');
+    $('td div img').css('opacity' , '0');
+    $('p.home_title').css('opacity' , '0');
+    $('td').animate({'background-position': '0px'}, 1400);
+    $('td').animate({'background-position': '370px'}, 1400);
+    setTimeout(function(){
+      switch (website) {
+        case 1:
+          location.href = "https://bananachen.github.io/Web-design/banana.html";
+          break;
+        case 2:
+          location.href = "http://www.csie.ncku.edu.tw/ncku_csie/images/ncku/photo/student.png";
+          break;
+        case 3:
+        location.href = "http://www.sbs.com.au/topics/sites/sbs.com.au.topics/files/gettyimages-619254150.jpg";
+            break;
+        case 4:
+          location.href = "https://www.facebook.com/csieweek/";
+            break;
+        case 5:
+          location.href = "https://www.facebook.com/chen.s.chi";
+            break;
+        default:
+      }
+    }, 2200);
+  }
+}
